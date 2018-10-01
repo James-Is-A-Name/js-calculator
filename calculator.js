180d439cac7a86274f231c52aed179fffa55af37
James <42189301+James-Is-A-Name@users.noreply.github.com>
2018-10-01 18:26:00 +1300

    add inital button event listeners

--- calculator.js
+++ calculator.js
@@ -1,2 +1,40 @@
 
 
+
+
+document.addEventListener("DOMContentLoaded",startCalc);
+
+function startCalc(){
+
+    addNumberButtonEvents();
+
+    addDecimalButtonEvent();
+
+    addPercentButtonEvent();
+
+    addOperationButtonEvent();
+
+    addClearButtonEvent();
+
+}
+
+
+function addNumberButtonEvents(){
+    let numbersButtonList = document.getElementsByClassName("digit");
+    
+}
+
+function addDecimalButtonEvent(){
+}
+
+function addPercentButtonEvent(){
+}
+
+function addOperationButtonEvent(){
+    
+}
+
+function addClearButtonEvent(){
+}
+
+
