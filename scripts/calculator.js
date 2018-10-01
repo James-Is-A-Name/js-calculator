83d5f7715486b0185e6b724aabf695c98ffc30b1
ptorrsmith <peter@ptorrsmith.com>
2018-10-01 19:22:59 +1300

    Move calc js to scripts directory

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,8 @@
 
+var displayOutput = "";
 
-
+var currentValue = "";
+var currentTotal = 0;
 
 document.addEventListener("DOMContentLoaded",startCalc);
 
@@ -15,14 +17,47 @@ function startCalc(){
     addOperationButtonEvent();
 
     addClearButtonEvent();
-
 }
+
+function updateDisplay(){
+    document.getElementById("numDisplay").innerHTML = displayOutput;
+}
+
+function appendDigit(newDigit){
+
+    currentValue += newDigit;
+
+    displayOutput = currentValue;
+
+    currentValue = "" + parseFloat(currentValue);
+    
+    updateDisplay();
+}
+
+function equate(){
+
+    //TODO ADD ALTERNATE OPERATORS = - .....
+
+    if(currentValue !== ""){
+        currentTotal += parseFloat(currentValue);
+    }
+
+    currentValue = "";
+
+    displayOutput = "" + currentTotal;
+    
+    updateDisplay();
+}
+
+
+
+
 
 
 function addNumberButtonEvents(){
     let numbersButtonList = document.getElementsByClassName("digit");
-    
 }
+
 
 function addDecimalButtonEvent(){
 }
